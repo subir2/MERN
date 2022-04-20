8f64c9e4c5d44622372c7fd4700e79e78c249c31
subir2 <subirsarker1995@gmail.com>
2022-04-20 15:04:58 +0600

    done till order

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { getAllProducts, createProduct,updateProduct,getProductDetails,deleteProduct } = require("../controllers/productController");
+const { getAllProducts,createProduct,updateProduct,getProductDetails,deleteProduct, createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts);
-router.route("/product/new").post(createProduct);
+router.route("/products").get(getAllProducts);
+router
+  .route("/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
 router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 
 module.exports = router;
